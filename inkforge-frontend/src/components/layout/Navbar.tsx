0d2df236d2187723d6d6b15f60c648a9f63a72db
DevershTechmero <devershtechmero@gmail.com>
2026-03-18 18:18:52 +0530

    change style in header logo

--- inkforge-frontend/src/components/layout/Navbar.tsx
+++ inkforge-frontend/src/components/layout/Navbar.tsx
@@ -20,8 +20,8 @@ export function Navbar() {
       <div className="container mx-auto flex h-16 items-center justify-between px-4">
         <Link href="/" className="flex items-center gap-2">
           {/* <Droplet className="h-7 w-7 text-primary" /> */}
-          <span className="font-display text-2xl font-bold text-foreground tracking-normal">
-            TatooInkify
+          <span className="flex justify-center items-center font-display text-2xl font-bold text-foreground tracking-widest">
+            Tatoo Inkify
           </span>
         </Link>
 
